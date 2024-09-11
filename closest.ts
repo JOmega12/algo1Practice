ac7f94818e8b27be309ddfce1e05bab492285734
Jensen <omega90ej@gmail.com>
2024-09-11 10:51:30 -0700

    finished

--- closest.ts
+++ closest.ts
@@ -1,6 +1,21 @@
 export function closest(nums: number[], target: number): number {
    // Implement me
-   return 0;
+
+   let closestNumber = nums[0] + nums[1] + nums[2];
+
+   for(let i = 0; i< nums.length - 2 ; i++){
+      for(let j = i + 1; j<nums.length - 1; j++){
+         for(let k = j + 1; k< nums.length; k++){
+            let newSum = nums[i] + nums[j] + nums[k];
+
+            if(Math.abs(newSum - target) < Math.abs(closestNumber -target)){
+               closestNumber = newSum
+            }
+         }
+      }
+   }
+   return closestNumber
+   // return 0;
 }
 
 module.exports = { closest }
